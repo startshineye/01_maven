d437af48bce927a4c6e133735639f8e013df68e7
startshineye <13001955858@163.com>
2019-12-25 23:21:58 +0800

    oa-flow

---
+++ oa-flow/src/test/java/com/yxm/oa/flow/ApplicationServiceTest.java
@@ -0,0 +1,31 @@
+package com.yxm.oa.flow;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.yxm.oa.flow.model.Application;
+import com.yxm.oa.flow.service.ApplicationService;
+
+@ContextConfiguration(locations= {"classpath:application-flow.xml"})
+@RunWith(SpringJUnit4ClassRunner.class)
+public class ApplicationServiceTest {
+	
+	@Autowired
+	private ApplicationService applicationService;
+	
+	@Test
+	public void testCreate() {
+		Application application = new Application();
+		
+		application.setEmployeeId(1L);
+		application.setDays(5L);
+		applicationService.create(application);
+		assertEquals(true, application.getId()>0);
+	}
+
+}
